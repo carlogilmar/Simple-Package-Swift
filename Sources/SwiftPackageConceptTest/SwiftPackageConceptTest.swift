a826cbcac557a278919094728e56a3310ee336bd
carlogilmar <carlogilmar12@gmail.com>
2018-05-11 01:48:11 -0500

    Adding simple class

--- Sources/SwiftPackageConceptTest/SwiftPackageConceptTest.swift
+++ Sources/SwiftPackageConceptTest/SwiftPackageConceptTest.swift
@@ -1,11 +1,8 @@
 import Foundation
-class FileReader {
- static func read(fileName: String) -> String? {
-   let fileManager = FileManager.default
-   let currentDirectoryURL = URL(fileURLWithPath:
-                          fileManager.currentDirectoryPath)
-   let fileURL = currentDirectoryURL.appendingPathComponent(fileName)
-   return try? String(contentsOf: fileURL, encoding: .utf8)
+
+class Example{
+ func sayHello() -> String? {
+	 return "Hola"
  }
 }
 
