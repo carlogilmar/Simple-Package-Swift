c76bce89452aeec481143b5cbfc33ec597af95cb
carlogilmar <carlogilmar12@gmail.com>
2018-05-11 01:41:44 -0500

    Adding test

--- Tests/SwiftPackageConceptTestTests/SwiftPackageConceptTestTests.swift
+++ Tests/SwiftPackageConceptTestTests/SwiftPackageConceptTestTests.swift
@@ -6,7 +6,8 @@ final class SwiftPackageConceptTestTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftPackageConceptTest().text, "Hello, World!")
+        XCTAssertEqual(FileReader.read(fileName: "hello.txt"), "Hello World")
+
     }
 
 
