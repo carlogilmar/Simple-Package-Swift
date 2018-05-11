054a8773d650f98c6f99c5314a6b351a46bc0d4d
carlogilmar <carlogilmar12@gmail.com>
2018-05-11 01:48:36 -0500

    Making my first great and fucking and beatiful unit test with Swifta Tests/SwiftPackageConceptTestTests/SwiftPackageConceptTestTests.swift

--- Tests/SwiftPackageConceptTestTests/SwiftPackageConceptTestTests.swift
+++ Tests/SwiftPackageConceptTestTests/SwiftPackageConceptTestTests.swift
@@ -3,10 +3,8 @@ import XCTest
 
 final class SwiftPackageConceptTestTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FileReader.read(fileName: "hello.txt"), "Hello World")
+        let example = Example()
+        XCTAssertEqual(example.sayHello(), "Hola")
 
     }
 
